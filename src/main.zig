e5af7311663d3687f9475aa1e3c60e1927f1e8e9
RetroDev256 <ericpetersen3.14@gmail.com>
2024-10-31 19:01:40 -0600

    make the sample multithreaded

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 const root = @import("root.zig");
 
-pub fn main() noreturn {
+pub fn main() !void {
+    const threads = try std.Thread.getCpuCount();
+    for (0..threads - 1) |_| {
+        _ = try std.Thread.spawn(.{}, perThread, .{});
+    }
+    perThread();
+}
+
+fn perThread() noreturn {
     const writer = std.io.getStdOut().writer();
     var buffer: [std.mem.page_size]u8 = undefined;
 
